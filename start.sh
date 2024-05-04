eea3622fb587cec51fdc697b3d69fc161c13a9a0
Ayan Singh <89607636+xx-limbo-096@users.noreply.github.com>
2024-05-04 10:36:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/xx-limbo-096/Infinite_files/Infinite_files
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /Infinite_files
 fi
-cd /Advance-File-Share-Bot
+cd /Infinite_files
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
